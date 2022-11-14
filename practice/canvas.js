129119bc28ad7f20f8225846d7317a4eef2cfe21
Willfred <beningaw@s.dcsdk12.org>
2022-11-14 15:45:52 -0700

    The best me

--- practice/canvas.js
+++ practice/canvas.js
@@ -1,68 +1,47 @@
-let canvas= document.getElementById("canvas-1");
-canvas.width=100;
-canvas.hight=100;
+let canvas = document.getElementById("canvas-1");
+canvas.width = 100;
+canvas.height = 100;
+/** @type {CanvasRenderingContext2d} */let context = canvas.getContext("2d");
+let squares = [];
+let colors = ["red", "orange", "yellow", "green", "blue", "indigo", "violet"];
+let colSize = 80;
+let rowSize = 40;
+let gridSize = 4;
+let size = canvas.width/gridSize;
+function drawSquare(x, y, color, size = 25) {
+	let square = new Path2D();
+	context.fillStyle = color; 
 
-/** @type{CanvasRenderingContext2d} */
-let context = canvas.getContext("2d");
+class ClickBox{ 
+	constructor(x, y, size, color){
+	 this.x=x;
+	 this.y=y; 
+	 this.size=size; 
+	 this.isClicked = false; 
+	 this.refreshRate = 500;
+	 this.lastRefresh =0; 
+	 this.color = "red"
+	{
 
-context.fillRECT(0,0,25,25); 
-context.fillStyle = "red";  
-(0,0,25,25); 
-let square = [];
-let gridSize = 4; 
-let size=canvas.width / gridSize  
-let colors = ["red", "orang", "yellow", "green", "blue", "indigo", "violet",]
-
-// All of these incrementers do the same 
-// row = row + 1 
-// row += 1
-// row++
-for(let row =0; row < gridSize; row = row+ 1){
-	for(let row =0; row < gridSize; row = row+ 1){
-		let colorIndex =  Math.floor (Math.random() * colors.length);
-		let color = colors(colorsIndex);
-		drawSquare(col * size, row, 0, "red", size ); 
-	}
-}
-
-// drawSquare(0, 0, "red");
-// drawSquare(25, 0, "blue");
-// drawSquare(50, 0, "green");
-// drawSquare(75, 0, "purple");
-
-function drawSquare(x, y,color, size=25){
-	let square= new Path2d();
-	square.rect(x, y, size, size);
-	
-	square.push(square);  
-	
-	context.fillStyle=color; 
-context.fillRect(x, y, size, size); 
-
+		
 } 
 
-console.log(squares); 
 
 
-function drawLoop()
-   for(let row =0; row < gridSize; row = row+ 1){
-	for(let row =0; row < gridSize; row = row+ 1){
-		let colorIndex =  Math.floor (Math.random() * colors.length);
-		let color = colors(colorsIndex);
-		drawSquare(col * size, row, 0, "red", size ); 
 	
-	
-	
-	
-	
-	
-	
-	
-	} 
-
-
-	requestAnimationFrame(drawLoop);  
+context.fillRect(x, y, 25, 25);
 	
 
+     
 
-requestAnimationFrame(drawLoopp;
+ function drawLoop() {
+	for(let row = 0; row < rowSize; row++) {
+		for(let col = 0; col < colSize; col++) {
+			let colorIndex = Math.floor(Math.random() * colors.length);
+			let color = colors[colorIndex];
+			drawSquare(col * size, row * size, color, size);
+
+		}
+	}requestAnimationFrame(drawLoop)
+}
+requestAnimationFrame(drawLoop);
